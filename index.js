915586822bcd2cf6d47b2cd5e5664bfaeb92c123
apoorvnema <apoorvnema38@gmail.com>
2024-04-03 20:43:48 +0530

    Creating path response with http server

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("Apoorv Nema");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node JS Test</title></head>');
+    res.write('<body>');
+    if (req.url === '/') {
+        res.write('<h1>Welcome to my Node Js project</h1>');
+    }
+    if (req.url === '/home') {
+        res.write('<h1>Welcome home</h1>');
+    }
+    if (req.url === '/about') {
+        res.write('<h1>Welcome to About Us page</h1>');
+    }
+    if (req.url === '/node') {
+        res.write('<h1>Welcome to my Node Js project</h1>');
+    }
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 })
 
-server.listen(4000);
+server.listen(3000);
