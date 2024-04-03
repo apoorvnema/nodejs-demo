f5d8caebc0dc07dab93c3a4aa7ccaf9bfbb4f94b
apoorvnema <apoorvnema38@gmail.com>
2024-04-04 00:18:05 +0530

    Writing in file using fs and HTML form

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Node JS Test</title></head>');
-    res.write('<body>');
-    if (req.url === '/') {
-        res.write('<h1>Welcome to my Node Js project</h1>');
+
+    if (url === '/' && method === 'GET') {
+        let file;
+        try {
+            file = fs.readFileSync('message.txt', 'utf8');
+        } catch (e) {
+            file = "File not found or could not be read.";
+        }
+
+        const html = `
+      <html>
+        <head><title>Writing in file using form</title></head>
+        <body>
+          <p>${file}</p>
+          <form method='POST' action='/message'>
+            <input type='text' name='message'><button type='submit'>Send</button>
+          </form>
+        </body>
+      </html>
+    `;
+        res.write(html);
+        return res.end();
     }
-    if (req.url === '/home') {
-        res.write('<h1>Welcome home</h1>');
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedData = Buffer.concat(body).toString();
+            const message = parsedData.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     }
-    if (req.url === '/about') {
-        res.write('<h1>Welcome to About Us page</h1>');
-    }
-    if (req.url === '/node') {
-        res.write('<h1>Welcome to my Node Js project</h1>');
-    }
-    res.write('</body>');
-    res.write('</html>');
+
+    res.statusCode = 404;
+    res.write("<h1>Not Found</h1>");
     res.end();
-})
+});
 
 server.listen(3000);
